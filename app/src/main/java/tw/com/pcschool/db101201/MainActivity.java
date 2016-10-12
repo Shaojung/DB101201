0fe852b31c2ee7be0fe03f66e2b7c0536c4dd7c5
Shaojung <abc@abc.com>
2016-10-12 19:27:24 +0800

    Switch two fragment.

--- app/src/main/java/tw/com/pcschool/db101201/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/db101201/MainActivity.java
@@ -1,5 +1,7 @@
 package tw.com.pcschool.db101201;
 
+
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        FragmentTransaction ft = getFragmentManager().beginTransaction();
+//        ft.add(R.id.layout_frag, new FragmentA());
+//        ft.commit();
     }
     public void click1(View v)
     {
-
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.layout_frag, new FragmentA());
+        ft.commit();
     }
     public void click2(View v)
     {
-
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.layout_frag, new FragmentB());
+        ft.commit();
     }
 }
